80e4d7f978da8ca5e5f08f4b956d2152933ecbdc
parastoo-hashemi <parastoohashemi78@gamil.com>
2026-01-28 12:27:10 +0100

    push

--- src/backend/server.js
+++ src/backend/server.js
@@ -5,7 +5,6 @@ import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 import crypto from "crypto";
-
 // Resolve __dirname in ESM context
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
